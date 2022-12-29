16e3f4d2bbcb714a0ba147c8cefcd05c21bc2509
abc@123.com <abc@123.com>
2022-12-29 15:02:30 +0530

    whole project with build file as docs

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    ContactComponent,
+    AboutComponent,
+    WorkComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
